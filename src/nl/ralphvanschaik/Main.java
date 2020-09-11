d94780b3c682837f66050f9fc7b3c85a40c347ad
Ralph van Schaik <ralph.van.schaik@me.com>
2020-09-11 09:05:37 +0200

    tweede commit, niet echt veel veranderd.

--- src/nl/ralphvanschaik/Main.java
+++ src/nl/ralphvanschaik/Main.java
@@ -3,6 +3,6 @@ package nl.ralphvanschaik;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here spinning
+        System.out.println("Hallo master Michel !");
     }
 }
